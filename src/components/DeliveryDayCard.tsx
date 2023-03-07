5e3b4dd67769f64ea43f5578c870265c2894b7f6
Damien Sedgwick <damienksedgwick@gmail.com>
2023-03-07 01:35:17 +0000

    wip: delivery day card is pretty much done

--- src/components/DeliveryDayCard.tsx
+++ src/components/DeliveryDayCard.tsx
@@ -1,3 +1,7 @@
+import { format } from "date-fns";
+import van from "assets/van.svg";
+import calendar from "assets/calendar.svg";
+
 interface DeliveryDayCardProps {
   showModal: boolean;
   toggleModal: () => void;
@@ -5,19 +9,35 @@ interface DeliveryDayCardProps {
 }
 
 export const DeliveryDayCard = ({
-  showModal,
   toggleModal,
   deliveryDayDate,
 }: DeliveryDayCardProps) => {
   console.log(deliveryDayDate);
 
   return (
-    <div>
-      <p>Delivery Day Card</p>
-      <p>{deliveryDayDate.toDateString()}</p>
-      <button onClick={toggleModal} style={{ margin: "1rem" }}>
-        {showModal ? "Close" : "Open"}
-      </button>
+    <div className="delivery-day-card">
+      <div className="card-header">
+        <p>
+          Choose your delivery <br />
+          day <span>Delivery is always free</span>
+        </p>
+      </div>
+      <div className="card-body">
+        <div className="card-body-left">
+          <p>{format(deliveryDayDate, "EEE MMMM dd")}</p>
+          <div>
+            <img src={van} alt="A small illustrative van icon" />
+            <small>Earliest delivery</small>
+          </div>
+        </div>
+        <div className="card-body-right">
+          <button onClick={toggleModal}>
+            <img src={calendar} alt="A small illustrative van icon" />
+            <small>{`${format(deliveryDayDate, "dd")}`}</small>
+            <span>Change &gt;</span>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
